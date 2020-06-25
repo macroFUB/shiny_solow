f4ed428e529a3a91ab7e646b86415d85791a6edf
jmossadnik <39995554+jmossadnik@users.noreply.github.com>
2020-06-25 17:04:15 +0200

    Removed A
    Removed technology A in order to facilitate things

--- shinySolow.R
+++ shinySolow.R
@@ -4,29 +4,26 @@ library(tidyverse)
 ui <- fluidPage(
   titlePanel("Solow-Modell"),
   withMathJax(helpText(
-    "Cobb-Douglas-Produktionsfunktion: \n $$AK^\\alpha+L^{1-\\alpha}$$"
+    "Cobb-Douglas-Produktionsfunktion:\n $$K^\\alpha+(L\\times E)^{1-\\alpha}$$"
   )),
   sidebarLayout(
     sidebarPanel(
       numericInput(inputId = "k", label = "Maximum von k", value = 0, min = 0),
       sliderInput(
         inputId="alpha", label="alpha", value = 0.5, min = 0.01, max = 0.99
-        ),
-      sliderInput(
-        inputId = "a", label = "A", value = 1, min = 0.01, max = 9.99
-        ),
+      ),
       sliderInput(
         inputId = "s", label = "s", value = 0.3, min = 0.01, max = 0.99
-        ),
+      ),
       sliderInput(
         inputId = "delta", label = "delta", value = 0.3, min = 0.01, max = 0.99
-        ),
+      ),
       sliderInput(
         inputId = "n", label = "n", value = 0.3, min = 0.01, max = 0.99
-        ),
+      ),
       sliderInput(
         inputId = "g", label = "g", value = 0.3, min = 0.01, max = 0.99
-        ),
+      ),
       tags$head(tags$style(
         type = 'text/css',
         'form.well { max-height: 600px; overflow-y: auto; }'
@@ -35,10 +32,10 @@ ui <- fluidPage(
     mainPanel(
       plotOutput('graph', click = "plot_click"),
       fluidRow(
-        column(width = 6,h4("steady state"), tableOutput('table')),
+        column(width = 6, h4("steady state"), tableOutput('table')),
         column(
           width = 6, h4("click values"), verbatimTextOutput("click_info")
-          ),
+        ),
       ),
       tableOutput("info")
     )
@@ -48,16 +45,16 @@ ui <- fluidPage(
 server <- function(input, output){
   dat <- reactive({
     max_k <-
-      (input$a / (input$delta + input$n + input$g)) ** (1 / (1 - input$alpha))
+      (1 / (input$delta + input$n + input$g)) ** (1 / (1 - input$alpha))
     k <- seq(0, ifelse(input$k == 0, max_k*2, input$k), 0.01)
-    y <- input$a * k ** input$alpha
+    y <- k ** input$alpha
     sy <- input$s * y
     dk <- (input$delta + input$n + input$g) * k
     k_star <-
       (
-        (input$s * input$a) / (input$delta + input$n + input$g)
+        (input$s) / (input$delta + input$n + input$g)
       ) ** (1 / (1 - input$alpha))
-    y_star <- input$a*(k_star ** input$alpha)
+    y_star <- (k_star ** input$alpha)
     data_table <- data.frame(k, y, sy, dk, k_star, y_star)
     data_table
   })
@@ -70,7 +67,7 @@ server <- function(input, output){
       geom_segment(
         aes(x = k_star, y = 0, xend = k_star, yend = y_star),
         linetype = "dashed", colour = "black"
-        ) +
+      ) +
       geom_segment(
         aes(x = 0, y = y_star, xend = k_star, yend = y_star),
         linetype = "dashed", colour = "black"
@@ -81,7 +78,7 @@ server <- function(input, output){
   output$click_info <- renderPrint({
     cat("input$plot_click:\n")
     cx <- as.numeric(input$plot_click["x"])
-    cy <- input$a * (cx ** (1/(1 - input$alpha)))
+    cy <- (cx ** (1/(1 - input$alpha)))
     cs <- input$s * cy
     cd <- (input$delta + input$g + input$n) * cx
     data.frame(
@@ -98,8 +95,7 @@ server <- function(input, output){
       "y" = tmp$y_star,
       "c" = tmp$y_star * (1 - input$s),
       "s" = tmp$y_star * input$s
-      )[1, ]
+    )[1, ]
   })
 }
 shinyApp(ui = ui, server = server)
-
